cfeb724cb5ea319fe9960ccaaf8c37c50126a38d
HappyAlfred <HappyAlfred@users.noreply.github.com>
2018-05-01 23:10:11 +0800

    Add files via upload

--- src/main/java/ckathode/weaponmod/render/RenderRocketShell.java
+++ src/main/java/ckathode/weaponmod/render/RenderRocketShell.java
@@ -9,11 +9,11 @@ import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 
 import ckathode.weaponmod.WeaponModResources;
-import ckathode.weaponmod.entity.projectile.EntityCrossbowBolt;
+import ckathode.weaponmod.entity.projectile.EntityRocketShell;
 
-public class RenderCrossbowBolt extends Render
+public class RenderRocketShell extends Render
 {
-	public void renderCrossbowBolt(EntityCrossbowBolt entityarrow, double d, double d1, double d2, float f, float f1)
+	public void renderDynamite(EntityRocketShell entityarrow, double d, double d1, double d2, float f, float f1)
 	{
 		bindEntityTexture(entityarrow);
 		GL11.glPushMatrix();
@@ -23,47 +23,47 @@ public class RenderCrossbowBolt extends Render
 		Tessellator tessellator = Tessellator.instance;
 		int i = 0;
 		float f2 = 0.0F;
-		float f3 = 0.5F;//(16/32)
+		float f3 = 0.5F;
 		float f4 = (0 + i * 10) / 32F;
 		float f5 = (5 + i * 10) / 32F;
 		float f6 = 0.0F;
-		float f7 = 0.15625F;//(5/32)
+		float f7 = 0.15625F;
 		float f8 = (5 + i * 10) / 32F;
 		float f9 = (10 + i * 10) / 32F;
 		float f10 = 0.05625F;
 		GL11.glEnable(32826 /*GL_RESCALE_NORMAL_EXT*/);
-		float f11 = entityarrow.arrowShake - f1;
+		float f11 = -f1;
 		if (f11 > 0.0F)
 		{
 			float f12 = -MathHelper.sin(f11 * 3F) * f11;
 			GL11.glRotatef(f12, 0.0F, 0.0F, 1.0F);
 		}
-		GL11.glRotatef(45F, 1.0F, 0.0F, 0.0F);//将旋转矩阵与当前矩阵相乘
-		GL11.glScalef(f10, f10, f10);//将一般的比例矩阵与当前矩阵相乘
-		GL11.glTranslatef(-4F, 0.0F, 0.0F);//将变换矩阵与当前矩阵相乘
-		GL11.glNormal3f(f10, 0.0F, 0.0F);//法向量
+		GL11.glRotatef(45F, 1.0F, 0.0F, 0.0F);
+		GL11.glScalef(f10, f10, f10);
+		GL11.glTranslatef(-4F, 0.0F, 0.0F);
+		GL11.glNormal3f(f10, 0.0F, 0.0F);
 		tessellator.startDrawingQuads();
-		tessellator.addVertexWithUV(-4D, -2D, -2D, f6, f8);//纹理(0,5/32)
-		tessellator.addVertexWithUV(-4D, -2D, 2D, f7, f8);//纹理(5/32,5/32)
-		tessellator.addVertexWithUV(-4D, 2D, 2D, f7, f9);//纹理(5/32,10/32)
-		tessellator.addVertexWithUV(-4D, 2D, -2D, f6, f9);//纹理(0,10/32)
+		tessellator.addVertexWithUV(-7D, -2D, -2D, f6, f8);
+		tessellator.addVertexWithUV(-7D, -2D, 2D, f7, f8);
+		tessellator.addVertexWithUV(-7D, 2D, 2D, f7, f9);
+		tessellator.addVertexWithUV(-7D, 2D, -2D, f6, f9);
 		tessellator.draw();
 		GL11.glNormal3f(-f10, 0.0F, 0.0F);
 		tessellator.startDrawingQuads();
-		tessellator.addVertexWithUV(-4D, 2D, -2D, f6, f8);
-		tessellator.addVertexWithUV(-4D, 2D, 2D, f7, f8);
-		tessellator.addVertexWithUV(-4D, -2D, 2D, f7, f9);
-		tessellator.addVertexWithUV(-4D, -2D, -2D, f6, f9);
+		tessellator.addVertexWithUV(-7D, 2D, -2D, f6, f8);
+		tessellator.addVertexWithUV(-7D, 2D, 2D, f7, f8);
+		tessellator.addVertexWithUV(-7D, -2D, 2D, f7, f9);
+		tessellator.addVertexWithUV(-7D, -2D, -2D, f6, f9);
 		tessellator.draw();
 		for (int j = 0; j < 4; j++)
 		{
 			GL11.glRotatef(90F, 1.0F, 0.0F, 0.0F);
 			GL11.glNormal3f(0.0F, 0.0F, f10);
 			tessellator.startDrawingQuads();
-			tessellator.addVertexWithUV(-5D, -2D, 0.0D, f2, f4);//纹理(0,0)
-			tessellator.addVertexWithUV(5D, -2D, 0.0D, f3, f4);//纹理(16/32,0)
-			tessellator.addVertexWithUV(5D, 2D, 0.0D, f3, f5);//纹理(16/32,5/32)
-			tessellator.addVertexWithUV(-5D, 2D, 0.0D, f2, f5);//纹理(0,5/32)
+			tessellator.addVertexWithUV(-8D, -2D, 0.0D, f2, f4);
+			tessellator.addVertexWithUV(8D, -2D, 0.0D, f3, f4);
+			tessellator.addVertexWithUV(8D, 2D, 0.0D, f3, f5);
+			tessellator.addVertexWithUV(-8D, 2D, 0.0D, f2, f5);
 			tessellator.draw();
 		}
 		
@@ -74,12 +74,12 @@ public class RenderCrossbowBolt extends Render
 	@Override
 	public void doRender(Entity entity, double d, double d1, double d2, float f, float f1)
 	{
-		renderCrossbowBolt((EntityCrossbowBolt) entity, d, d1, d2, f, f1);
+		renderDynamite((EntityRocketShell) entity, d, d1, d2, f, f1);
 	}
 	
 	@Override
 	protected ResourceLocation getEntityTexture(Entity entity)
 	{
-		return WeaponModResources.Textures.BOLT;
+		return WeaponModResources.Textures.ROCKETSHELL;
 	}
 }
